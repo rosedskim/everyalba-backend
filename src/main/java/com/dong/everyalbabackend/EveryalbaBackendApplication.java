3b638ebaf72ee0fb82711c02d5c7c3ab6fc07d7d
rosedskim <rosedskim@naver.com>
2022-08-29 01:54:46 +0900

    Initialize project

---
+++ src/main/java/com/dong/everyalbabackend/EveryalbaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dong.everyalbabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryalbaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryalbaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dong/everyalbabackend/EveryalbaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dong.everyalbabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryalbaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
